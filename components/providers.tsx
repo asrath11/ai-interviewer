bcf5802508862c391b43561db14b900b8f92df8c
Asrath <pasrath.2004@gmail.com>
2025-09-23 11:58:16 +0530

    added theme toggle

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,22 +1,12 @@
 'use client';
 
-import { useState, useEffect } from 'react';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => setMounted(true), []);
-
-  if (!mounted) {
-    // prevent hydration errors
-    return null;
-  }
-
   return (
     <SessionProvider>
-      <ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
+      <ThemeProvider attribute='class' defaultTheme='system' enableColorScheme>
         {children}
       </ThemeProvider>
     </SessionProvider>
